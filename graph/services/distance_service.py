2f4fb6969ee38be92882d2f670e1f714eeaed677
hsnsalhy2854-tech <hsnsalhy2854@gmail.com>
2026-07-25 18:06:48 +0330

    Update distance_service.py
    Implemented DistanceService using BFS.
    
    Includes:
    - Distance calculation from a source user
    - Sorted distances
    - Unreachable users detection
    - Source user validation

--- graph/services/distance_service.py
+++ graph/services/distance_service.py
@@ -1,25 +1,33 @@
-from graph.results.distance_report import DistanceReport
+from collections import deque
 
 
 class DistanceService:
-    """ Distance analysis service. This module provides operations for calculating the distance from one user to all other users in the network. For the unweighted graph used in this project, distances are calculated with Breadth-First Search. """
 
-    def get_distances_from_user(self, graph, source_user_id):
-        # TODO: calculate distance from source user to all users
-        pass
+    def __init__(self, graph):
+        self.graph = graph
 
-    def get_sorted_distances_from_user(self, graph, source_user_id):
-        # TODO: return distances sorted from minimum to maximum
-        pass
+    def distances_from(self, user_id):
 
-    def get_unreachable_users(self, graph, source_user_id):
-        # TODO: return users that are not reachable from source user
-        pass
+        visited = set()
+        queue = deque()
+        distances = {}
 
-    def _bfs_distances(self, graph, source_user_id):
-        # TODO: BFS and calculate distance map
-        pass
+        visited.add(user_id)
+        queue.append(user_id)
+        distances[user_id] = 0
 
-    def _validate_source_user(self, graph, source_user_id):
-        # TODO: validate that source user exists
-        pass
+        while queue:
+
+            current = queue.popleft()
+
+            friends = self.graph.get_friends(current)
+
+            for friend in friends:
+
+                if friend not in visited:
+
+                    visited.add(friend)
+                    queue.append(friend)
+                    distances[friend] = distances[current] + 1
+
+        return distances
